9dc26ed4e02fa4628e0039e513e525f274bb350a
jerry800416 <39479411+jerry800416@users.noreply.github.com>
2020-12-13 17:07:26 +0800

    Add files via upload
    修正無法爬取問題

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 from pyquery import PyQuery as pq
 import json
 
-
 # read mixerbox backup playlist
-with open('./mixerbox_library.txt','r') as file:
+with open('./mixerbox_library.txt','r',encoding="utf-8") as file:
     result = json.loads(file.read())
-
 
 for i in result:
     # read my list
@@ -14,23 +12,15 @@ for i in result:
         for playlist in i['content']:
             # this is playlist title
             playlist_name = (playlist['name'])
-            with open('./'+playlist_name+'.txt','a') as f : 
+            with open('./'+playlist_name+'.txt','a',encoding='utf-8') as f : 
                 for playlist_music in playlist['content']:
                     # playlist content is url code
                     url = 'https://www.youtube.com/watch?v='+playlist_music
                     # pyquery
                     doc = pq(url=url,encoding='utf-8')
                     # get youtube title
-                    title = doc('html head title').text()
-                    try_again = 0
-                    # can't catch youtube title 
-                    while title == 'YouTube':
-                        try_again += 1
-                        if try_again <= 3 :
-                            doc = pq(url=url,encoding='utf-8')
-                            title = doc('html head title').text()
-                        else :
-                            break
-                    if title != 'YouTube':
+                    title = doc('meta[name=title]').attr('content')
+                    print(title)
+                    if title != '':
                         # write youtube title to playlist txt
-                        f.write(title[:-10]+'\r\n')
+                        f.write(title+'\r')
